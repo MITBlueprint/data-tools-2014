c11a9a845904c7a140a1cfb909b5e5ea3b9ad55c
Michael Holachek <michael.holachek@gmail.com>
2014-01-11 13:01:13 -0500

    Added map visualization! Yay

--- HS_latlong.py
+++ HS_latlong.py
@@ -1,23 +1,38 @@
 # -*- coding: utf-8 -*-
 # search for high school locations on Google
 
+CSVDATA = "../blueprint.csv"
+OUTPUTFILE = "highschools.js"
+
 highschools = []
 highschool_locations = {}
-areacodes = []
-fancy_words = ["best", "excellence", "specialized", "magnet", "premier", "recognition", "top-performing"]
+highschool_count = {}
+# areacodes = []
+# fancy_words = ["best", "excellence", "specialized", "magnet", "premier", "recognition", "top-performing"]
 
 import csv, re, wikipedia, bs4
 
 # gather a list of high schools from Wufoo CSV form data
-with open('blueprint.csv', 'rb') as csvfile:
+with open(CSVDATA, 'rb') as csvfile:
 		registrationreader = csv.reader(csvfile, delimiter=",", quotechar="|")
 		for row in registrationreader:
 				highschoolname = row[5]
+
+				# make sure it's not homeschool
+				homeschool = re.findall(r'[.]*(homeschool|Home school|Home School|Homeschool)[.]*', highschoolname)
+				if homeschool:
+					continue
+
 				if highschoolname not in highschools:
-					if re.match(r' (school|SCHOOL|School)[ ]?$', highschoolname):
-						highschools.append(row[5])
-					else:
-						highschools.append('"' + row[5][1:-1] + ' School"')
+					highschool_count[highschoolname] = 1
+				else:
+					highschool_count[highschoolname] = highschool_count.get(highschoolname) + 1
+					
+				school = re.findall(r'[.]*(school|School|Highschool|Academy|College|Institute)[.]*', highschoolname)
+				if school:
+					highschools.append(row[5])
+				else:
+					highschools.append('"' + row[5][1:-1] + ' School"')
 				# areacode = re.sub("[^0-9]", "", row[4])
 				# if areacode[:1] == "1":
 				# 	areacode = areacode[1:4]
@@ -29,32 +44,56 @@ with open('blueprint.csv', 'rb') as csvfile:
 # 1. location
 # 2. "excellence" level -- let's see how many times it says "best" or "excellence" vs "growing" and "improvement"
 
-for highschool in highschools[10:11]:
+for highschool in highschools[2:]:
 	try:
-		print highschool
 		wikisearch = wikipedia.search(highschool)
-		print wikisearch
 		wikipage = wikipedia.page(wikisearch[0]).html()
 	except:
 		continue
 	try:
-		latlong = re.findall(r'[0-9]{2}\.[0-9]{3,10}', wikipage)
-		highschool_locations[highschool] = latlong[0] + "N," + latlong[1] + "W"
-		print(highschool + " // " + latlong[0] + ", " + latlong[1])
+		latitude = re.findall(r'[3-5][0-9]\.[0-9]{3,10}', wikipage)
+		longitude = re.findall(r'[7-9,1][0-9]{1,2}\.[0-9]{3,10}', wikipage)
+		try:
+			highschool_locations[highschool] = latitude[0] + ",-" + longitude[0]
+			# print(highschool + " // " + latlong[0] + ", " + latlong[1])
+		except:
+			print("Uh oh, no lat or long found for " + highschool + ". Continuing...")
+			continue
 	except AttributeError:
 		try:			
 			zipcode = re.findall(r' [0-9]{5}', wikiparse.prettify())[0][1:]
 			highschool_locations[highschool] = zipcode
-			print(highschool + " // " + zipcode)
+			# print(highschool + " // " + zipcode)
 		except:
 			try:
 				wikiparse = bs4.BeautifulSoup(wikipage)
 				locality = wikiparse.find('span', attrs={'class': 'locality'}).text
 				region = wikiparse.find('span', attrs={'class': 'region'}).text
 				highschool_locations[highschool] = locality + " " + region
-				print(highschool + " // " + locality + " " + region)
+				# print(highschool + " // " + locality + " " + region)
 			except:
 				print("Couldn't find " + highschool + " on Wikipedia.")
 
-print("\n\n\n=================\nHigh school locations: \n\n")
-print highschool_locations
+
+# print formatted json for Goog Maps
+
+# highschools['High School Name'] = {
+# center: new google.maps.LatLng(LATITUDE, LONGITUDE),
+# count: COUNT	
+# };
+
+print("Scraping finished. Writing file...")
+
+outputfile = open(OUTPUTFILE, 'w')
+
+outputfile.write("var highschools = {};\n\n")
+
+for highschool, count in highschool_count.items():
+	if highschool_locations.get(highschool):
+		outputfile.write("highschools['" + str(highschool[1:-1]) + "'] = { center: new google.maps.LatLng(" + str(highschool_locations.get(highschool)) + "), count: " + str(count) + " };\n")
+
+outputfile.close()
+
+print("\nDone!")
+# print("\n\n\n=================\nHigh school locations: \n\n")
+# print highschool_locations
---
+++ highschools.js
@@ -0,0 +1,15 @@
+var highschools = {};
+
+highschools['Medgar Evers College Preparatory School'] = { center: new google.maps.LatLng(40.7195,-73.9485), count: 1 };
+highschools['International Academy'] = { center: new google.maps.LatLng(42.60306,-83.22583), count: 1 };
+highschools['Jenks High School'] = { center: new google.maps.LatLng(36.024619,-95.96829), count: 1 };
+highschools['Quabbin Regional High School'] = { center: new google.maps.LatLng(42.40000,-72.11472), count: 1 };
+highschools['Jack M. Barrack Hebrew Academy'] = { center: new google.maps.LatLng(40.018,-75.327), count: 1 };
+highschools['Newburyport High School'] = { center: new google.maps.LatLng(42.81306,-70.88611), count: 1 };
+highschools['Monta Vista High School'] = { center: new google.maps.LatLng(37.3145,-122.0567), count: 1 };
+highschools['Conestoga High School'] = { center: new google.maps.LatLng(40.0472,-75.4533), count: 3 };
+highschools['A. Philip Randolph Campus High School'] = { center: new google.maps.LatLng(40.818281,-73.950199), count: 1 };
+highschools['Northern Highlands Regional High School'] = { center: new google.maps.LatLng(41.034822,-85.165), count: 2 };
+highschools['Syosset High school'] = { center: new google.maps.LatLng(40.8308278,-73.4829139), count: 1 };
+highschools['Germantown Friends School'] = { center: new google.maps.LatLng(40.03278,-75.17167), count: 1 };
+highschools['Montgomery Blair High School'] = { center: new google.maps.LatLng(39.01806,-77.01139), count: 1 };
